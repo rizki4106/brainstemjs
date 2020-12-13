e5231bc75f057b3b2baf58a9b4cff06e84e21d01
rizki4106 <rizkimaulana4106@gmail.com>
2020-12-13 14:13:04 +0700

    fix accuracy score

--- metrics/acuracy_score.js
+++ metrics/acuracy_score.js
@@ -1,7 +1,7 @@
 function accuracy_score(x1 = [], x2 = []){
     let similar = 0
     for(let i = 0; i < x1.length; i++){
-        if(x1[i] == x2[i]){
+        if(x1[i][0] === x2[i][0]){
             similar += 1
         }
     }
